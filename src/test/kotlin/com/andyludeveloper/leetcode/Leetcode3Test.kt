3e9a1fba4aaf96471f4ac91c8ced77afad966f5d
Andy Lu <alu@futuredial.com>
2020-12-14 17:27:52 +0800

    Leetcode 3: longest substring without repeating characters - initial commit

---
+++ src/test/kotlin/com/andyludeveloper/leetcode/Leetcode3Test.kt
@@ -0,0 +1,20 @@
+package com.andyludeveloper.leetcode
+
+import org.junit.Test
+
+class Leetcode3Test {
+    @Test
+    fun `length 0 should return 0`() {
+        val sut = Leetcode3()
+        val result = sut.lengthOfLongestSubstring("")
+        assert(result == 0)
+    }
+
+}
+
+class Leetcode3 {
+    fun lengthOfLongestSubstring(s: String): Int {
+        return 0
+    }
+
+}
