7d4929b0d3d86491a99f4c1f1f54e8c6c9b02a68
Andy Lu <alu@futuredial.com>
2020-12-14 17:37:21 +0800

    Leetcode 3: longest substring without repeating characters - all characters is same return 1

--- src/test/kotlin/com/andyludeveloper/leetcode/Leetcode3Test.kt
+++ src/test/kotlin/com/andyludeveloper/leetcode/Leetcode3Test.kt
@@ -10,10 +10,24 @@ class Leetcode3Test {
         assert(result == 0)
     }
 
+    @Test
+    fun `all characters is same should return 1`() {
+        val sut = Leetcode3()
+        val result = sut.lengthOfLongestSubstring("bbbbb")
+        assert(result == 1)
+    }
 }
 
 class Leetcode3 {
     fun lengthOfLongestSubstring(s: String): Int {
+        if(s.isNotEmpty()){
+            val map = HashMap<Char, Int>()
+            for (i in s){
+                var value = map.getOrDefault(i, 0)
+                map[i] = ++value
+            }
+            return map.size
+        }
         return 0
     }
 
