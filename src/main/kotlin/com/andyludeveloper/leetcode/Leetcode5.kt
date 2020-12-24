17ecf5e99c4a282b004847c105ce31ded9b7d1e1
Andy Lu <alu@futuredial.com>
2020-12-24 22:16:32 +0800

    Leetcode 5: Init commit - single character: pass

--- src/main/kotlin/com/andyludeveloper/leetcode/Leetcode5.kt
+++ src/main/kotlin/com/andyludeveloper/leetcode/Leetcode5.kt
@@ -2,7 +2,34 @@ package com.andyludeveloper.leetcode
 
 class Leetcode5 {
     fun longestPalindrome(s: String): String {
-        return s
-    }
+        var result = ""
 
+        for(i in s.indices){
+            for(j in i+1 .. s.length){
+                val subString = s.subSequence(i, j).toString()
+                if(isPalindrome(subString)){
+                    if(result.isEmpty()){
+                       result = subString
+                    }else{
+                        if(subString.length > result.length){
+                            result = subString
+                        }
+                    }
+                }
+            }
+        }
+        return result
+        }
+}
+fun isPalindrome(s: String): Boolean {
+    var l = 0
+    var r = s.length - 1
+    while (l <= r) {
+        if (s[l] != s[r]) {
+            return false
+        }
+        l += 1
+        r -= 1
+    }
+    return true
 }
--- src/test/kotlin/com/andyludeveloper/leetcode/Leetcode5Test.kt
+++ src/test/kotlin/com/andyludeveloper/leetcode/Leetcode5Test.kt
@@ -1,10 +1,8 @@
 package com.andyludeveloper.leetcode
 
 import org.junit.Test
-import kotlin.test.expect
 
 //Longest Palindromic Substring
-
 class Leetcode5Test {
     @Test
     fun `single character`() {
@@ -12,4 +10,25 @@ class Leetcode5Test {
         val result = sut.longestPalindrome("a")
         assert(result == "a")
     }
+
+    @Test
+    fun `two characters`() {
+        val sut = Leetcode5()
+        val result = sut.longestPalindrome("ab")
+        assert(result == "a")
+    }
+
+    @Test
+    fun `four characters cbbd should return bb`() {
+        val sut = Leetcode5()
+        val result = sut.longestPalindrome("cbbd")
+        assert(result == "bb")
+    }
+    @Test
+    fun `four characters babad should return bab`() {
+        val sut = Leetcode5()
+        val result = sut.longestPalindrome("babad")
+        assert(result == "bab")
+    }
+
 }
